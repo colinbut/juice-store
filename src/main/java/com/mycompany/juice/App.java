3503772c20992ed767c76cd5bd2cc69ac7ec60ac
Colin But <colinbut@users.noreply.github.com>
2016-09-24 21:28:05 +0100

    clean up of maven generated code

--- src/main/java/com/mycompany/juice/App.java
+++ src/main/java/com/mycompany/juice/App.java
@@ -1,13 +1,11 @@
 package com.mycompany.juice;
 
 /**
- * Hello world!
- *
+ * @author colin
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Juice Store");
     }
 }
