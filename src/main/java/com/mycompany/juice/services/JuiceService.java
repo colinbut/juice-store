bf377c7372d925bd79f63aefed6e88ea4090461f
Colin But <colinbut@users.noreply.github.com>
2016-09-25 21:16:17 +0100

    implemented services

---
+++ src/main/java/com/mycompany/juice/services/JuiceService.java
@@ -0,0 +1,25 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.juice.services;
+
+import com.mycompany.juice.model.Juice;
+
+import java.util.List;
+
+public interface JuiceService {
+
+    List<Juice> getJuiceList();
+
+    Juice getJuiceByName(String name);
+
+    Juice getJuiceById(int id);
+
+    void addJuice(Juice juice);
+
+    void deleteJuiceByName(String name);
+
+    void deleteJuiceById(int id);
+}
---
+++ src/main/java/com/mycompany/juice/services/JuiceServiceImpl.java
@@ -0,0 +1,52 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.juice.services;
+
+import com.google.inject.Inject;
+import com.mycompany.juice.dao.JuiceDao;
+import com.mycompany.juice.model.Juice;
+
+import java.util.List;
+
+public class JuiceServiceImpl implements JuiceService {
+
+    private JuiceDao juiceDao;
+
+    @Inject
+    public JuiceServiceImpl(JuiceDao juiceDao) {
+        this.juiceDao = juiceDao;
+    }
+
+    @Override
+    public List<Juice> getJuiceList() {
+        return juiceDao.getJuices();
+    }
+
+    @Override
+    public Juice getJuiceByName(String name) {
+        return juiceDao.getJuice(name);
+    }
+
+    @Override
+    public Juice getJuiceById(int id) {
+        return juiceDao.getJuice(id);
+    }
+
+    @Override
+    public void addJuice(Juice juice) {
+        juiceDao.addJuice(juice);
+    }
+
+    @Override
+    public void deleteJuiceByName(String name) {
+        juiceDao.removeJuice(name);
+    }
+
+    @Override
+    public void deleteJuiceById(int id) {
+        juiceDao.removeJuice(id);
+    }
+}
