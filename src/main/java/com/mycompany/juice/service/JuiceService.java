407fbaf127b1e8ce8edde801c3c1d9717cd59961
Colin But <colinbut@users.noreply.github.com>
2016-09-27 22:41:19 +0100

    changed package name service

--- src/main/java/com/mycompany/juice/service/JuiceService.java
+++ src/main/java/com/mycompany/juice/service/JuiceService.java
@@ -3,7 +3,7 @@
  * | Copyright © 2016 Colin But. All rights reserved. 
  * |-------------------------------------------------
  */
-package com.mycompany.juice.services;
+package com.mycompany.juice.service;
 
 import com.mycompany.juice.model.Juice;
 
